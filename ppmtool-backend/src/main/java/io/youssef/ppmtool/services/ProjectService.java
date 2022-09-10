b24cb57db72475e49cd20e98b6186e95b6677222
youssef-ettafssaoui <yettafssaoui2@gmail.com>
2022-09-10 18:29:38 +0100

    Find Project by the Identifier

--- ppmtool-backend/src/main/java/io/youssef/ppmtool/services/ProjectService.java
+++ ppmtool-backend/src/main/java/io/youssef/ppmtool/services/ProjectService.java
@@ -19,6 +19,12 @@ public class ProjectService {
         } catch (Exception e) {
             throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists !");
         }
-
+    }
+    public Project findProjectByIdentifier(String projectId) {
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        if (project == null) {
+            throw new ProjectIdException("Project ID '"+projectId+"' does not exist !");
+        }
+        return project;
     }
 }
