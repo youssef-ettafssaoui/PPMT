88e01d7075d2d6be95f4a4bf7f023fe3d224970d
youssef-ettafssaoui <yettafssaoui2@gmail.com>
2022-09-11 22:14:36 +0100

    React Router, first Functional component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,18 @@ import './App.css';
 import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
 import "bootstrap/dist/css/bootstrap.min.css";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import AddProject from './components/Project/AddProject';
 
 function App() {
   return (
-    <div className="App">
-        <Header />
-        <Dashboard />
-    </div>
+    <Router>
+      <div className="App">
+          <Header />
+          <Route exact path="/addProject" component={AddProject} />
+          <Route exact path="/dashboard" component={Dashboard} />
+      </div>
+    </Router>
     );
 }
 
