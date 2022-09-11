57e6e068e11bf74bb16c365fc3b000f3fb731a07
youssef-ettafssaoui <yettafssaoui2@gmail.com>
2022-09-11 23:48:16 +0100

    AddProject Component - controlled form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
 import "bootstrap/dist/css/bootstrap.min.css";
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import AddProject from './components/Project/AddProject';
 
 function App() {
