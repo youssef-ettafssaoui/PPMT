392c541e1e6bab77a8d32791b45b66af03cc4b04
youssef-ettafssaoui <yettafssaoui2@gmail.com>
2022-09-14 19:39:31 +0100

    Create Project from React-1

--- frontend/src/components/Project/AddProject.js
+++ frontend/src/components/Project/AddProject.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import { PropTypes } from "prop-types";
+import { connect } from "react-redux";
+import { createProject } from "../../actions/projectActions";
 
 class AddProject extends Component {
   constructor() {
@@ -99,4 +102,8 @@ class AddProject extends Component {
   }
 }
 
-export default AddProject;
+// AddProject.propTypes = {
+//   createProject = PropTypes.func.isRequired
+// }
+
+export default connect(null, { createProject })(AddProject);
